734f1308e338a8b95e992ec3f1dab070a47818a3
Dan Pickett <dpickett@enlightsolutions.com>
2011-01-23 15:49:29 -0500

    logging and configuration

---
+++ lib/postmaster_general.rb
@@ -0,0 +1,35 @@
+require "actionmailer"
+
+require 'fileutils'
+
+module PostmasterGeneral do
+  def self.log_directory=(path)
+    configatron.postmaster_general.log_directory = path
+  end
+
+  def self.log_directory
+    configatron.postmaster_general.log_directory
+  end
+
+  def self.log_deliveries(path, &block)
+    before_index = ActionMailer::Base.deliveries.size - 1
+    before_index = 0 if before_index < 0
+
+    yield
+
+    log_path = log_directory.join(path)
+
+    FileUtils.mkdir_p(File.dirname(log_path))
+    FileUtils.rm_f(log_path)
+
+    ActionMailer::Base.deliveries[before_index..-1].each do |mail|
+      File.open(log_path, "w+") do |f|
+        f << "=================="
+        f << mail.encoded
+        f << "=================="
+      end
+    end
+  end
+end
+  
+
