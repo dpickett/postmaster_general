8da39b419125f6deef47069826ca7c5058643a7d
Dan Pickett <dpickett@enlightsolutions.com>
2011-01-23 16:14:37 -0500

    doc

--- lib/postmaster_general.rb
+++ lib/postmaster_general.rb
@@ -3,14 +3,18 @@ require 'configatron'
 require 'fileutils'
 
 module PostmasterGeneral
+  #sets the log directory for actionmailer logging
   def self.log_directory=(path)
     configatron.postmaster_general.log_directory = path
   end
 
+  #returns the log directory configured previously
   def self.log_directory
     configatron.postmaster_general.log_directory
   end
 
+  #logs mail deliveries performed within the specified block to the specified path
+  #the path is appended to the previously configured log_directory
   def self.log_deliveries(path, &block)
     before_index = ActionMailer::Base.deliveries.size - 1
     before_index = 0 if before_index < 0
